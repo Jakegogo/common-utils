6a93ad384255b78beff72a508fbcb09063834e7e
Jake <335923591@qq.com>
2016-06-02 14:00:13 +0800

    json转换工具类添加方法

--- src/main/java/com/jake/common/util/JsonUtils.java
+++ src/main/java/com/jake/common/util/JsonUtils.java
@@ -3,6 +3,9 @@ package com.jake.common.util;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+
+import java.lang.reflect.Type;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,6 +45,16 @@ public abstract class JsonUtils {
 	 * @return T
 	 */
 	public static <T> T jsonString2Object(String jsonString, Class<T> valueType) {
+		return JSON.parseObject(jsonString, valueType);
+	}
+	
+	/**
+	 * json字符串转换成对象
+	 * @param jsonString String
+	 * @param valueType 对象类型
+	 * @return T
+	 */
+	public static <T> T jsonString2Object(String jsonString, Type valueType) {
 		return JSON.parseObject(jsonString, valueType);
 	}
 	
